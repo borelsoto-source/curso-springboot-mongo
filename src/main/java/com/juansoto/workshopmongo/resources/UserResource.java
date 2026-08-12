6bf9c0b83e75405c85047d586c048d0d642a72c2
Juan Soto <borel.soto@gmail.com>
2026-08-12 17:50:38 -0300

    Obtendo o usuario por id

--- src/main/java/com/juansoto/workshopmongo/resources/UserResource.java
+++ src/main/java/com/juansoto/workshopmongo/resources/UserResource.java
@@ -5,9 +5,7 @@ import com.juansoto.workshopmongo.dto.UserDTO;
 import com.juansoto.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,5 +23,11 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
--- src/main/java/com/juansoto/workshopmongo/services/UserService.java
+++ src/main/java/com/juansoto/workshopmongo/services/UserService.java
@@ -2,6 +2,8 @@ package com.juansoto.workshopmongo.services;
 
 import com.juansoto.workshopmongo.domain.User;
 import com.juansoto.workshopmongo.repository.UserRepository;
+import com.juansoto.workshopmongo.services.exception.ObjectNotFoundException;
+import com.sun.jdi.ObjectCollectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    public User findById(String id){
+        return repository.findById(id)
+            .orElseThrow(()-> new ObjectNotFoundException("Usuario nao encontrado para o id: "+id));
     }
 }
